684920a8dc3e97ea327637b4455a4e38f211f61a
Lary <lary96bk@gmail.com>
2023-05-04 18:42:06 +0300

    fix

--- server.js
+++ server.js
@@ -3,18 +3,12 @@ const app = require('./app')
 mongoose.Promise = global.Promise
 const { PORT, DB_HOST } = process.env
 
-const connection = mongoose.connect(DB_HOST, {
-	// promiseLibrary: global.Promise,
-	// useCreateIndex: true,
-	// useUnifiedTopology: true,
-	// useFindAndModify: false,
-})
+const connection = mongoose.connect(DB_HOST)
 
 connection
 	.then(() => {
-		app.listen(PORT, function () {
-			console.log(`Database connection successful`)
-		})
+		console.log(`Database connection successful`)
+		app.listen(PORT, () => console.log('Server is launched'))
 	})
 	.catch(err => {
 		console.log(`Server not running. Error message: ${err.message}`)
